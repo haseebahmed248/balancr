330fc233e9cce9fdec1b200960ea5f000d2b7146
haseebahmed248 <haseebahmed6157@gmail.com>
2026-01-16 18:36:43 +0500

    Basic proxy - forwards requests to single backend

--- cmd/balancr/main.go
+++ cmd/balancr/main.go
@@ -1,7 +1,26 @@
-package balancr
+package main
 
-import "log"
+import (
+	"balancr/internal/proxy"
+	"log"
+	"net"
+)
+
+func startServer() {
+	listener, err := net.Listen("tcp", "localhost:7000")
+	if err != nil {
+		log.Print(err)
+	}
+	log.Print("Listening to port 7000")
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+		}
+		go proxy.SetupProxy(conn)
+	}
+}
 
 func main() {
-	log.Print("Load Balancr")
+	startServer()
 }
--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -1,2 +1,19 @@
 // request forwarding
 package proxy
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func SetupProxy(clientConn net.Conn) {
+	conn, err := net.Dial("tcp", "localhost:9090")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer conn.Close()
+	go io.Copy(conn, clientConn)
+	io.Copy(clientConn, conn)
+}
